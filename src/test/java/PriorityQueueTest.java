0a2c3bb1874783d39535b878913dbebebf967635
wabson0201 <wabson0201@gmail.com>
2021-04-04 19:20:16 +0800

    Fixed Wrong benchmark

--- src/test/java/PriorityQueueTest.java
+++ src/test/java/PriorityQueueTest.java
@@ -14,11 +14,11 @@ import static org.junit.jupiter.api.Assertions.*;
 public class PriorityQueueTest {
     static Stream<Arguments> stringIntAndListProvider() {
         return Stream.of(
-                Arguments.of(new int[]{3,1,2}, new int[]{99,1,2,3}),
-                Arguments.of(new int[]{-3,-1,-2,5}, new int[]{99,-3,-2,-1,5}),
-                Arguments.of(new int[]{3,-2,-5,-1,2}, new int[]{99,-5,-2,-1,2,3}),
-                Arguments.of(new int[]{-3,1,11,0,9,3}, new int[]{99,-3,0,1,3,9,11}),
-                Arguments.of(new int[]{3,7,2,-1,-2}, new int[]{99,-2,-1,2,3,7})
+                Arguments.of(new int[]{3,1,2}, new int[]{3,2,1}),
+                Arguments.of(new int[]{-3,-1,-2,5}, new int[]{5,-2,-1,-3}),
+                Arguments.of(new int[]{3,-2,-5,-1,2}, new int[]{3,-2,-1,2,-5}),
+                Arguments.of(new int[]{-3,1,11,0,9,3}, new int[]{11,0,1,3,9,-3}),
+                Arguments.of(new int[]{3,7,2,-1,-2}, new int[]{7,-1,2,3,-2})
         );
     }
     @ParameterizedTest(name="#{index} - Test with Argument={0},{1}")
